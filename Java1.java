c0a1b50db619184f88f5d7bb8f5182e244e629ab
ReemaAloumi <67341048+ReemaAloumi@users.noreply.github.com>
2020-06-24 08:25:14 +0300

    GROUPD

--- Java1.java
+++ Java1.java
@@ -1,11 +1,12 @@
 public class Java1 {
-    public static void main(String[] args) {
-        int x =4;
-        int y =3;
-        mystery(x,y);
-        System.out.println(mystery(x,y));
-     }
-     public static int mystery(int n, int a)
+    public static void main(final String[] args) {
+        final int x = 4;
+        final int y = 3;
+        mystery(x, y);
+        System.out.println(mystery(x, y));
+    }
+
+    public static int mystery(final int n, final int a)
  
  {
   if (n == 1) return a;
